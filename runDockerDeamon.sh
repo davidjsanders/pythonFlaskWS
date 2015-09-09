b7ed53965536b56763390bef3061e26c9740413d
dsandersAzure <dsanders_can@outlook.com>
2015-09-09 17:03:34 +0000

    Updated runDockerDaemon.sh

--- runDockerDeamon.sh
+++ runDockerDeamon.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name "phoneWS" -p 5000:5000 -P -d dsanders/phonews /bin/bash -c "/phonews/phoneWS.py"
+docker run -p 5000:5000 -P -d dsanders/phonews /bin/bash -c "/phonews/phoneWS.py"
